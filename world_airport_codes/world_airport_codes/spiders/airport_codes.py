c93e0601916cf9fbd3c232779e0a0b65c8549c12
wawanridwan <wawan.ridone@gmail.com>
2019-05-03 17:17:43 +0700

    [BRANCH WAWANRIDWAN] update scrapy

---
+++ world_airport_codes/world_airport_codes/spiders/airport_codes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class AirportCodesSpider(scrapy.Spider):
+    name = 'airport_codes'
+    allowed_domains = ['http://www.world-airport-codes.com/alphabetical/airport-name/a.html']
+    start_urls = ['http://www.world-airport-codes.com/alphabetical/airport-name/a.html']
+
+    def parse(self, response):
+        data = []
+        table = response.css('.stack2')
+        row_selector = ".// tr[ @class ='light-row']|.// tr[@ class ='dark-row']"
+        for row in table.xpath(row_selector):
+            name = row.xpath('.th/a/text()').extract_first()
+            type_ = row.xpath('.td[1]/text()[2]').extract_first().strip()
+            data.append({
+            'name': name,
+            'type': type_,
+
+                })
+        return data
