a4086ea916a8b31899caeafed424ddbb2b4af7af
wawanridwan <wawan.ridone@gmail.com>
2019-05-03 17:29:52 +0700

    [BRANCH WAWANRIDWAN] Paket Komplit Scrapy World Airport Codes

--- world_airport_codes/world_airport_codes/spiders/airport_codes.py
+++ world_airport_codes/world_airport_codes/spiders/airport_codes.py
@@ -10,12 +10,23 @@ class AirportCodesSpider(scrapy.Spider):
     def parse(self, response):
         data = []
         table = response.css('.stack2')
-        row_selector = ".// tr[ @class ='light-row']|.// tr[@ class ='dark-row']"
+        row_selector = ".//tr[@class ='light-row']|.//tr[@class ='dark-row']"
         for row in table.xpath(row_selector):
-            name = row.xpath('.th/a/text()').extract_first()
-            type_ = row.xpath('.td[1]/text()[2]').extract_first().strip()
+            name = row.xpath('./th/a/text()').extract_first()
+            type_ = row.xpath('./td[1]/text()[2]').extract_first().strip()
+            city = row.xpath('./td[2]/text()').extract_first()
+            country = row.xpath('./td[3]/text()').extract_first()
+            iata = row.xpath('./td[4]/text()').extract_first()
+            icao = row.xpath('./td[5]/text()').extract_first()
+            faa = row.xpath('./td[6]/text()').extract_first()
+
             data.append({
-            'name': name,
-            'type': type_,
+                'name': name,
+                'type': type_,
+                'city': city,
+                'country': country,
+                'iata': iata,
+                'icao': icao,
+                'faa': faa
                 })
         return data
